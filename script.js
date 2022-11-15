81cb35228df17253b2af18364436c2bb4ad58e23
Azeez Wariz <warizazeez05@gmail.com>
2022-11-15 16:09:54 +0100

    conditionalsta

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let firstName = "Odebode"
+
+if(firstName =='Akeem')
+    document.getElementById("introname").innerHTML ="Linguistic"
+    
+
+else if(firstName =='Odebode')
+    document.getElementById("first").innerHTML ="Good at english"
+   
+else if(firstName =='Odebode')
+    document.getElementById("last").innerHTML ="Good in teaching"
+   
+
+else if(firstName =='Prudence')
+    document.getElementById("middle").innerHTML ="Too like money"
+    
+
+else document.getElementById("invalid").innerHTML ="invalid details"
+    
+    
