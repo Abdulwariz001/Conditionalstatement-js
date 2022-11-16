67ad963d5c2c41058f26b293bb231585445c0c20
Azeez Wariz <warizazeez05@gmail.com>
2022-11-16 16:36:45 +0100

    conditional project

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 
 let firstName = "Akeem"
 
-if(firstName == "Akeem")
+if(firstName == "Akeem"){
     document.getElementById("introname").innerHTML ="First class prevarsity"
-    
+}
 
-else if(firstName == "Odebode")
-    document.getElementById("first").innerHTML ="English tutor"
+else if(firstName == "Odebode"){
+    document.getElementById("first").innerHTML ="Master prudence"
+}
    
-else if(firstName == "odebode")
+else if(firstName == "odebode"){
     document.getElementById("last").innerHTML ="Education is the best legacy"
+}
+
+else if(firstName == "Prudence"){
+    document.getElementById("middle").innerHTML ="Mr Odebode Akeem"
+}
    
+else{ document.getElementById("invalid").innerHTML ="invalid details"
+} 
+ 
+    
+    
 
-else if(firstName == "Prudence")
-    document.getElementById("middle").innerHTML ="English language99"
-    
 
-else document.getElementById("invalid").innerHTML ="invalid details"
-    
-    
